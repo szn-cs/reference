7e95172251ffe6225977ef26c21e3ff71ce461e6
Safi <nassarsafi@gmail.com>
2020-07-15 21:15:32 +0300

    ➕ Algorithms folder for common algorithms implementations.

---
+++ algorithms/sortingArray.java
@@ -0,0 +1,32 @@
+// Simple quick implementation of array sorting.
+import java.util.*;
+
+public class Main {
+
+	public static int[] sort(int[] array) {
+		boolean sorted;
+		do {
+			sorted = true; // assumption
+			for(int i=0; i < array.length- 1; i++) {
+				if(array[i] > array[i+1]) {
+					int temp = array[i+1]; 
+					array[i+1] = array[i]; 
+					array[i] = temp;
+					sorted = false;
+				}					
+			}
+		} while(!sorted);
+		return array;
+	}
+	public static void main( String [] args) {
+		int[] a = new int[] {2,7,1,3,2,8};
+		
+		a = sort(a);
+			
+		for (int element: a) {
+			System.out.print(element + " ");
+		}
+
+	}	
+}
+
