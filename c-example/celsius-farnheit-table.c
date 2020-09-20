f28d9ae1971ec021576d8002f194e9942f5fd8e9
Safi <nassarsafi@gmail.com>
2020-09-20 09:27:49 +0300

    ✒ Add C exmaple

---
+++ c-example/celsius-farnheit-table.c
@@ -0,0 +1,30 @@
+/* Celsius-Franheit Conversion tables: using floating point numbers */
+
+#include <stdio.h> 
+
+void celsiusToFarenheit() {
+  float celsius, farenheit; 
+  int lower = 0, upper = 300, step = 20; // ranges of values with mathematical range: [lower, upper]
+ 
+  for(celsius = lower; celsius <= upper; celsius += step) {
+    farenheit = (9.0/5.0 * celsius) + 32.0;// formula conversion mathematically: F = 9/5 x C + 32
+    printf("%3.0f\t%6.1f\n", celsius, farenheit);
+  }
+}
+
+void farenheitToCelsius() {
+  float celsius, farenheit; 
+  int lower = 0, upper = 300, step = 20; // ranges of values with mathematical range: [lower, upper]
+
+  for(farenheit = lower; farenheit <= upper; farenheit += step) {
+    celsius = 5.0 / 9.0 * (farenheit - 32);// formula conversion mathematically: C = 5/9 * (F - 32)
+    printf("%3.0f\t%6.1f\n", farenheit, celsius);
+  }
+}
+
+int main() {
+  printf("\n%s\n", "Celsius to Farenheit Conversion Table:");
+  celsiusToFarenheit();
+  printf("\n%s\n", "Farenheit to Celsius Conversion Table:");
+  farenheitToCelsius();
+}
