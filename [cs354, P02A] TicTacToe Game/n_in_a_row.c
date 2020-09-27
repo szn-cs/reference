d346a5cf57fd197e5eab0a4e6cfad92b524637d5
Safi <nassarsafi@gmail.com>
2020-09-27 20:10:06 +0300

    [cs354, P02A] ✒ Complete main function and utility funcitons.

--- [cs354, P02A] TicTacToe Game/n_in_a_row.c
+++ [cs354, P02A] TicTacToe Game/n_in_a_row.c
@@ -18,14 +18,61 @@
 //
 // Persons:          
 //
-// Online sources:   
+// Online sources:   Enumeration C reference https://en.cppreference.com/w/c/language/enum
+//                   Error handling in C https://www.geeksforgeeks.org/error-handling-c-programs/
+//                   Error codes reference https://www.thegeekstuff.com/2010/10/linux-error-codes/
 ////////////////////////////////////////////////////////////////////////////////
    
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h> 
      
-char *DELIM = ",";  // commas ',' are a common delimiter character for data strings
+char *DELIM = ",";  // string containing the delimiter characters
+extern int errno; // error number from errno.h library
+
+// numerals representing board state validity
+enum STATE { 
+    IN_VALID = 0, 
+    VALID = 1 
+};
+// tokens representing the possible values the in game board. 
+enum MARK { 
+    EMPTY = 0 /*unmarked space*/, 
+    x = 1, 
+    o = 2 
+};  
+// define error codes. Strarting from 132 which won't conflict with errno.h library defined error codes
+enum ERROR_CODE { 
+    E_SIZE, 
+    E_ARGUMENT_MISSING, 
+    E_ARGUMENT_MULTIPLE, 
+};
+
+/**
+ * Retreive appropriate error message
+ * 
+ * e: code integral matching the enum ERROR_CODE definitions
+ * error message string.
+ */
+const char* getErrorMessage(enum ERROR_CODE e) {
+    char *m = NULL; // message that should be allocated on the heap to be returned as a pointer
+    //! Note: order of case values must match enum declaration declaration order.
+    switch(e) {
+        case E_SIZE: 
+            m = "Invalid board size parsed from input file";
+        break; 
+        case E_ARGUMENT_MISSING: 
+            m = "Missing filename argument";
+        break; 
+        case E_ARGUMENT_MULTIPLE: 
+            m = "Only a single filename argument must be provided";
+        break; 
+        default: 
+            m = "Unknown error code thrown";
+    }
+    return m;
+}
 
 /**
  * Tic-Tac-Toe Game - Usually a 3x3 grid board, allowing players to mark (either X or O) on the game board, until one player wins or there are no spaces available to mark. Winners should get 3 of their marks "in a row" either horizontally, vertically, or diagonally. 
@@ -39,13 +86,15 @@ char *DELIM = ",";  // commas ',' are a common delimiter character for data stri
 
 */
 
-void get_dimensions(FILE *fp, int *size);
+int get_dimensions(FILE *fp);
+void getMarks(FILE *fp, int **board, int size);
 int n_in_a_row(int **board, int size);  
-void clear2DArray(int **array, int rows, int columns); 
-void print2DArray(int **array, int rows, int columns);
+void clear2DArray(int **array, int row, int column); 
+void print2DArray(int **array, int *rows, int *columns, char *delimiter);
+void* freeNestedArrays(int **array, int row); 
+void throw(const char *m1, const char *m2);
 
-
-/* ❌
+/* 
  * This program processes a file containing the current game state, represented as a 2D grid of Xs and Os. 
  * It prints Valid if the input file contains a game board with either 1 or no winners (draw); 
  * and where there is at most 1 more X than O (#X = #O or #X = #O+1). 
@@ -59,83 +108,69 @@ void print2DArray(int **array, int rows, int columns);
         - either no winner or one winner; X and O cannot both be winners
         - either one winning line (i.e., row, column, or diagonal), or two winning lines that intersect on one mark; two parallel winning lines are invalid
  * 
- *  // TODO: program must check the return values for errors of the library functions, malloc(), fopen(), and fclose(). Handle errors by displaying an appropriate error message and then calling exit(1).
  * 
  * argc: command-line argument count
  * argv: command-line argument value
  */
 int main(int argc, char *argv[]) {              
-     
     // Verify number of input arguments
     if(argc != 2) {
-        char *str = (argc < 2) ? "Missing filename argument." : "Only a single filename argument must be provided."; 
-        printf("%s \n[Number of filename arguments provided: %d]\n", str, argc - 1); 
-        exit(1);
+        char *m = "Invalid number of filename arguments"; 
+        (argc < 2) ? throw(getErrorMessage(E_ARGUMENT_MISSING), m) : throw(getErrorMessage(E_ARGUMENT_MULTIPLE), m); 
     }
     
-
     //Open the file and check if it opened successfully.
-    FILE *fp = fopen(*(argv + 1), "r");
-    if (fp == NULL) {
-        printf("Can't open file for reading.\n");
-        exit(1);
-    }
+    char *filename = *(argv + 1); // get filename from input argument
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) throw(strerror(errno /*ENOENT*/), "Can't open file for reading");
 
-    //Declare local variables.
-    int size;
+    // game board dimensions (rows & columns), where total number of marks on the board will be in the range of 0 to size*size; 
+    int size = 0;   
+    // current board state validity
+    enum STATE state = IN_VALID;  
 
     //retrieve the board size.
-    get_dimensions(fp, &size);
+    size = get_dimensions(fp);
+    // validate input size value, range mathematically:  [3, 99]
+    if(size < 3 || size > 99)
+        throw(getErrorMessage(E_SIZE), "");
 
     // Create board with the matching dimesions, represnted by 2D heap allocated array.
     int **board = malloc(sizeof(int *) * size); 
-    for(int i = 0; i < size; i++)
+    if(board == NULL) throw(strerror(errno /*ENOMEM*/), "Failed to allocated memory.");
+    // create nested arrays
+    for(int i = 0; i < size; i++) {
         *(board + i) = malloc(sizeof(int) * size); 
+        if(*(board + i) == NULL) throw(strerror(errno /*ENOMEM*/), "Failed to allocated memory.");
+    }
     // initialize 2D array with zeros.
     clear2DArray(board, size, size); 
 
-    print2DArray(board, size, size);
+    // fill in-memory board with values from the file representation of the board marks
+    getMarks(fp, board, size);     
 
-    /*
-    //Read the file line by line. Assuming there are `size` lines where each line has `size` numbers (columns) separated by commas.
-    //Tokenize each line wrt the delimiter character to store the values in your 2D array.
-    char *line = NULL;
-    size_t len = 0;
-    char *token = NULL;
-    for (int i = 0; i < size; i++) {
+    // debug board
+    print2DArray(board, &size, &size, DELIM); 
 
-        if (getline(&line, &len, fp) == -1) {
-            printf("Error while reading the file.\n");
-            exit(1);
-        }
+    // 
+    state = n_in_a_row(board, size); 
 
-        // Assuming only 0, 1, or 2 will be the values in the game board. Tokens representation: 0 => unmarked space, 1 => X, 2 => O 
-        token = strtok(line, DELIM);
-        for (int j = 0; j < size; j++) {
-            //TODO: Complete the line of code below
-            //to initialize your 2D array.
-            //  <ADD ARRAY ACCESS CODE HERE>  = atoi(token);
-            token = strtok(NULL, DELIM);
-        }
+    // print game board validation message
+    switch(state) {
+        case VALID: 
+            printf("valid\n");
+        break; 
+        case IN_VALID: 
+        default: 
+            printf("invalid\n");
     }
 
-    //TODO: Call the function n_in_a_row and print the appropriate output depending on the function's return value.
-
-
-
-    //TODO: Free all dynamically allocated memory. program must properly free up all dynamically allocated memory at the end of the program.
+    // Free up dynamically allocated memory.
+    board = freeNestedArrays(board, size); // free associated arrays & return NULL
 
     //Close the file.
-    if (fclose(fp) != 0) {
-        printf("Error while closing the file.\n");
-        exit(1);
-    } 
+    if (fclose(fp) != 0) throw(strerror(errno /*EBADF or other*/), "Error while closing the file");     
 
-
-    // TODO: after printing error message call exit(1) if the user invokes the program incorrectly (for example, without any arguments, or with two or more arguments.
-     
-
-*/
     return 0;       
 }       
 
@@ -145,9 +180,9 @@ int main(int argc, char *argv[]) {
  * the size of the board (number of rows and columns).
  * 
  * fp: file pointer for input file
- * size: pointer to size
+ * extracted size of board game
  */
-void get_dimensions(FILE *fp, int *size) {      
+int get_dimensions(FILE *fp) {      
     char *line = NULL;
     size_t len = 0;
     if (getline(&line, &len, fp) == -1) {
@@ -157,49 +192,106 @@ void get_dimensions(FILE *fp, int *size) {
 
     char *token = NULL;
     token = strtok(line, DELIM);
-    *size = atoi(token);
+    return atoi(token);
 }
 
-/* ❌
- * Returns 1 if and only if the board is in a valid state.
- * Otherwise returns 0.
+/**
+ * Parse file line by line, extracting board marks, and filling them into the in-memory board array.
+ * Assuming there are `size` lines where each line has `size` numbers (columns) separated by commas. 
+ *  
+ * fp: file pointer for input file
+ * board: address of 2D array representing the board grid
+ * size: number of rows and columns
+ */
+void getMarks(FILE *fp, int **board, int size) {
+    char *line = NULL; // address of buffer containing the line text // TODO: buffer should be free up manually
+    size_t len = 0; // buffer size
+    char *token = NULL; // string of parsed portion
+    
+    for (int i = 0; i < size; i++) {
+        if (getline(&line, &len, fp) == -1) throw("Error while reading the file", "");
+        // Tokenize line wrt the delimiter character
+        token = strtok(line, DELIM);
+        // store user marks in the board array
+        for (int j = 0; j < size; j++) {
+            *(*(board + i) + j) = atoi(token); //! Note: atoi() function doesn't detect errors.
+            token = strtok(NULL, DELIM); // continue scanning string from the previous successful call
+        }
+    }
+}
+
+/* 
+ * Returns 1 if and only if the board is in a valid state, otherwise returns 0 (These values correspond to the enum STATE).
  * 
  * board: heap allocated 2D board
- * size: number of rows and columns; represting the game board size (square board), where total number of marks on the board will be in the range of 0 to size*size (i.e. dismension of board); Assuming `size` is a positive integer in the range of 3 to 99 inclusive, mathematically:  [3, 99]
+ * size: number of rows and columns; 
  */
 int n_in_a_row(int **board, int size) {
 
 
-    return 0;   
+    return IN_VALID;
+
+
+    return VALID;   
 } 
 
 /**
  * Set all elements of 2D array to zero. 
  * 
  * array: input 2-dimensional array of array. 
- * rows: # of array rows. 
- * columns: # of array columns
+ * row: # of array rows. 
+ * columns: pointer to # of array columns
  */ 
-void clear2DArray(int **array, int rows, int columns) {
-    for(int r = 0; r < rows; r++) 
-        for(int c = 0; c < columns; c++) 
-            *(*(array + r) + c) = 0;
+void clear2DArray(int **array, int row, int column) {
+    while(row-- > 0) 
+        while(column-- > 0) 
+            *(*(array + row) + column) = 0;
 }
 
 /**
- * print all elements in the 2D array, each row in a new line. 
+ * print all elements in the 2D array, each row in a separate line, and each column separated by a delimiter. 
  * 
  * array: input 2-dimensional array of array. 
- * rows: # of array rows. 
- * columns: # of array columns
+ * row: # of array rows. 
+ * column: # of array columns
+ * delimiter: separator symbol between columns
  */ 
-void print2DArray(int **array, int rows, int columns) {
-    for(int r = 0; r < rows; r++) {
-        for(int c = 0; c < columns; c++) {
-            printf("%i", *(*(array + r) + c) );
+void print2DArray(int **array, int *rows, int *columns, char *delimiter) {
+    for(int r = 0; r < *rows; r++) {
+        for(int c = 0; c < *columns; c++) {
+            // print current element with delimiter when required
+            printf("%i%s", *(*(array + r) + c), (c != *columns - 1) ? delimiter : ""); 
         }
         printf("\n");
     }
+    printf("\n");
+}
+
+/**
+ * Freeup nested arrays & parent array for a heap-allocated array of arrays.
+ * 
+ * array: input 2-dimensional array of array. 
+ * row: # of rows in the array.
+ * NULL to bea assigned to the array pointer.
+ */
+void* freeNestedArrays(int **array, int row) {
+    while(row-- > 0) {
+        free(array[row]); array[row] = NULL; // freeup memory of nested arrays
+    };
+    free(array); // free main/parent array
+    return NULL; // to be assigned to the pointer identifier scoped to the caller function.
+}
+
+/**
+ * Print supplied error messages and exit program with fail code 1
+ * 
+ * m1: Main code error message
+ * m2: Additional custom message (string) to be printed out along with the code error message.
+ */
+void throw(const char *m1, const char *m2) {
+    // print messages
+    printf("%s: %s;\n", m1, m2);
+    exit(1);
 }
 
 
