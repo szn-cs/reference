86092ba00dd12ec2f3f96bb2ae04d1ed6751a728
Safi <nassarsafi@gmail.com>
2020-12-03 07:47:49 +0200

    ✅ [cs300, P09]

--- [cs300, P09] Pokemon Catalog/src/PokemonNode.java
+++ [cs300, P09] Pokemon Catalog/src/PokemonNode.java
@@ -15,8 +15,9 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
+ * Provides a wrapper for Pokemon objects in the BST data structure used.
+ * 
  * @author Safi
- *
  */
 public class PokemonNode {
 
@@ -25,51 +26,62 @@ public class PokemonNode {
   private PokemonNode rightChild; // reference to the right child
 
   /**
-   * A one-argument constructor which sets leftChild and rightChild to null and initializes the data
-   * field.
+   * A one-argument constructor accepting a Pokemon instance field.
    * 
+   * @param data a particular Pokemon instance
    * @throws IllegalArgumentException if data is null
-   * @param data
    */
   public PokemonNode(Pokemon data) {
+    if (data == null)
+      throw new IllegalArgumentException("Invalid data argument passed - cannot be null");
+
+    this.data = data;
+    leftChild = rightChild = null; // initialize children nodes
   }
 
   /**
-   * Returns a reference to the left child of this PokemonNode
+   * Getter left child node
+   * 
+   * @return a reference to the left child of this PokemonNode
    */
   public PokemonNode getLeftChild() {
+    return leftChild;
   }
 
   /**
-   * Returns a reference to the right child of this PokemonNode
+   * Getter right child node
    * 
-   * @return
+   * @return a reference to the right child of this PokemonNode
    */
   public PokemonNode getRightChild() {
+    return rightChild;
   }
 
   /**
-   * Returns a reference to the Pokemon contained in this PokemonNode
+   * Getter Pokemon data instance
    * 
-   * @return
+   * @return a reference to the Pokemon contained in this PokemonNode
    */
   public Pokemon getPokemon() {
+    return data;
   }
 
   /**
    * Sets the left child of this PokemonNode (null values allowed)
    * 
-   * @param left
+   * @param left child node reference
    */
   public void setLeftChild(PokemonNode left) {
+    this.leftChild = left;
   }
 
   /**
    * Sets the right child of this PokemonNode (null values allowed)
    * 
-   * @param right
+   * @param right child node reference
    */
   public void setRightChild(PokemonNode right) {
+    this.rightChild = right;
   }
 
 }
--- [cs300, P09] Pokemon Catalog/src/PokemonTree.java
+++ [cs300, P09] Pokemon Catalog/src/PokemonTree.java
@@ -1,197 +1,253 @@
+//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
+//
+// Title: PokemonNode.java
+// Course: CS 300 Fall 2020
+//
+// Author: Safi Nassar
+// Email: nassar2@wisc.edu
+// Lecturer: Hobbes LeGault
+//
+///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+///////////////////////////////////////////////////////////////////////////////
 
-// File Header comes here
 import java.util.NoSuchElementException;
 
 /**
- * This class implements a binary search tree (BST) which stores a set of
- * Pokemons. The left subtree contains the Pokemons who are less powerful than
- * the Pokemon stored at a parent node. The right subtree contains the Pokemons
- * who are more powerful than the Pokemon stored at a parent node.
- *
+ * This class implements a binary search tree (BST) which stores a set of Pokemons. The left subtree
+ * contains the Pokemons who are less powerful than the Pokemon stored at a parent node. The right
+ * subtree contains the Pokemons who are more powerful than the Pokemon stored at a parent node.
+ * 
+ * @author Safi
  */
 public class PokemonTree {
-	private PokemonNode root; // root of this binary search tree
-	private int size; // total number of Pokemons stored in this tree.
+  private PokemonNode root; // root of this binary search tree
+  private int size; // total number of Pokemons stored in this tree.
 
-	/**
-	 * Checks whether this binary search tree (BST) is empty
-	 * 
-	 * @return true if this PokemonTree is empty, false otherwise
-	 */
-	public boolean isEmpty() {
-		// TODO Complete the implementation of this method.
-		return false; // remove this statement. A default return statement added to let this code
-		              // compile.
-	}
+  /**
+   * Checks whether this binary search tree (BST) is empty
+   * 
+   * @return true if this PokemonTree is empty, false otherwise
+   */
+  public boolean isEmpty() {
+    return size == 0 ? true : false;
+  }
 
-	/**
-	 * Returns the number of Pokemons stored in this BST.
-	 * 
-	 * @return the size of this PokemonTree
-	 */
-	public int size() {
-		// TODO Complete the implementation of this method.
-		return 0; // remove this statement. A default return statement added to let this code
-		          // compile.
-	}
+  /**
+   * Get the number of Pokemons stored in this BST.
+   * 
+   * @return the size of this PokemonTree
+   */
+  public int size() {
+    return size;
+  }
 
-	/**
-	 * Recursive helper method to add a new Pokemon to a PokemonTree rooted at
-	 * current.
-	 * 
-	 * @param current    The "root" of the subtree we are inserting new pokemon
-	 *                   into.
-	 * @param newPokemon The Pokemon to be added to a BST rooted at current.
-	 * @return true if the newPokemon was successfully added to this PokemonTree,
-	 *         false otherwise
-	 */
-	public static boolean addPokemonHelper(Pokemon newPokemon, PokemonNode current) {
-		// TODO Complete the implementation of this method.
-		return false; // remove this statement. A default return statement added to let this code
-		// compile.
-	}
+  /**
+   * Recursive helper method to add a new Pokemon to a PokemonTree rooted at current.
+   * 
+   * @param current    The "root" of the subtree we are inserting new pokemon into.
+   * @param newPokemon The Pokemon to be added to a BST rooted at current.
+   * @return true if the newPokemon was successfully added to this PokemonTree, false otherwise
+   */
+  public static boolean addPokemonHelper(Pokemon newPokemon, PokemonNode current) {
+    int comparison = current.getPokemon().compareTo(newPokemon);
 
-	/**
-	 * Adds a new Pokemon to this PokemonTree
-	 * 
-	 * @param newPokemon a new Pokemon to add to this BST.
-	 * @return true if the new was successfully added to this BST, and returns false
-	 *         if there is a match with this Pokemon already already stored in this
-	 *         BST.
-	 */
-	public boolean addPokemon(Pokemon newPokemon) {
-		// TODO Complete the implementation of this method.
-		if (isEmpty()) { // Add new to an empty PokemonTree
-		  // TODO /* MISSING CODE */
+    // base case: if a duplicate pokemon
+    if (comparison == 0)
+      return false;
 
-		} else { // Add new to an non-empty PokemonTree
-		  // TODO /* MISSING CODE */
-		  // [Hint]: call addPokemonHelper to help implement this behavior.
-		}
-		return false; // remove this statement. A default return statement added to let this code
-		              // compile.
-	}
+    // base case: no child in target subtree
+    if (comparison > 0 && current.getLeftChild() == null
+        || comparison < 0 && current.getRightChild() == null) {
+      // insert into the appropriate position
+      if (comparison > 0)
+        current.setLeftChild(new PokemonNode(newPokemon));
+      else
+        current.setRightChild(new PokemonNode(newPokemon));
+      return true;
+    }
 
-	/**
-	 * Recursive helper method which returns a String representation of the BST
-	 * rooted at current. An example of the String representation of the contents of
-	 * a PokemonTree is provided in the description of the above toString() method.
-	 * 
-	 * @param current reference to the current PokemonNode within this BST.
-	 * @return a String representation of all the Pokemons stored in the sub-tree
-	 *         PokemonTree rooted at current in increasing order with respect to the
-	 *         CP values. Returns an empty String "" if current is
-	 *         null.
-	 */
-	public static String toStringHelper(PokemonNode current) {
-		// TODO Complete the implementation of this method.
-		return null; // remove this statement.
-	}
+    // reduction case: when children present
+    return (comparison > 0) ? addPokemonHelper(newPokemon, current.getLeftChild())
+        : addPokemonHelper(newPokemon, current.getRightChild());
+  }
 
-	/**
-	 * Returns a String representation of all the Pokemons stored within this BST in
-	 * the increasing order, separated by a newline "\n". For instance:
-	 * "[Pikachu CP:123 (A:1 S:2 D:3)]" + "\n" + "[Eevee CP:224 (A:2 S:2 D:4)]" + "\n" + 
-	 * [Lapras CP:735 (A:7 S:3 D:5)] + "\n" + "[Mewtwo CP:999 (A:9 S:9 D:9)]" + "\n"
-	 * 
-	 * @return a String representation of all the Pokemons stored within this BST
-	 *         sorted in an increasing order with respect to the CP values.
-	 *         Returns an empty string "" if this BST is empty.
-	 */
-	public String toString() {
-		// TODO Complete the implementation of this method.
-		// [Hint]: call toStringHelper() method to help implement this behavior.
-		return null; // remove this statement.
-	}
+  /**
+   * Adds a new Pokemon to this PokemonTree
+   * 
+   * @param newPokemon a new Pokemon to add to this BST.
+   * @return true if the new was successfully added to this BST, and returns false if there is a
+   *         match with this Pokemon already already stored in this BST.
+   */
+  public boolean addPokemon(Pokemon newPokemon) {
+    if (newPokemon == null) // skip null enteries
+      return false;
 
-	/**
-	 * Search for a Pokemon (Pokemon) given the CP value as lookup key.
-	 * 
-	 * @param cp combat power of a Pokemon
-	 * @return the Pokemon whose CP value equals our lookup key.
-	 * @throws a NoSuchElementException with a descriptive error message if there is
-	 *           no Pokemon found in this BST having the provided CP value
-	 */
-	public Pokemon lookup(int cp) {
-		return lookupHelper(cp, root);
-	}
+    if (isEmpty()) { // Add new to an empty PokemonTree
+      root = new PokemonNode(newPokemon);
+      size++;
+      return true;
+    }
 
-	/**
-	 * Recursive helper method to lookup a Pokemon given a reference Pokemon with
-	 * the same CP in the subtree rooted at current
-	 * 
-	 * @param find    a reference to a Pokemon target we are lookup for a match in
-	 *                the BST rooted at current.
-	 * @param current "root" of the subtree we are looking for a match to find
-	 *                within it.
-	 * @return reference to the Pokemon stored stored in this BST which matches
-	 *         find.
-	 * @throws NoSuchElementException with a descriptive error message if there is
-	 *                                no Pokemon whose CP value matches target value,
-	 *                                stored in this BST.
-	 */
-	public static Pokemon lookupHelper(int cp, PokemonNode current) {
-		// TODO Complete the implementation of this method
-		return null; // remove this statement (added to let this code to compile)
-	}
+    // Add new to an non-empty PokemonTree
+    boolean status = addPokemonHelper(newPokemon, root); // add to node tree, and return operation
+                                                         // status
+    if (status)
+      size++;
+    return status;
+  }
 
-	/**
-	 * Computes and returns the height of this BST, counting the number of nodes
-	 * (PokemonNodes) from root to the deepest leaf.
-	 * 
-	 * @return the height of this Binary Search Tree
-	 */
-	public int height() {
-		return heightHelper(root);
-	}
+  /**
+   * Recursive helper method which returns a String representation of the BST rooted at current. An
+   * example of the String representation of the contents of a PokemonTree is provided in the
+   * description of the above toString() method.
+   * 
+   * @param current reference to the current PokemonNode within this BST.
+   * @return a String representation of all the Pokemons stored in the sub-tree PokemonTree rooted
+   *         at current in increasing order with respect to the CP values. Returns an empty String
+   *         "" if current is null.
+   */
+  public static String toStringHelper(PokemonNode current) {
+    if (current == null)
+      return "";
 
-	/**
-	 * Recursive helper method that computes the height of the subtree rooted at
-	 * current
-	 * 
-	 * @param current pointer to the current PokemonNode within a PokemonTree
-	 * @return height of the subtree rooted at current, counting the number of
-	 *         PokemonNodes
-	 */
-	public static int heightHelper(PokemonNode current) {
-		// TODO Complete this implementation of this method
-		return 0; // remove this statement.
-	}
+    String aggregator = ""; // build up the string
+    // left subtree
+    if (current.getLeftChild() != null)
+      aggregator += toStringHelper(current.getLeftChild());
 
-	/**
-	 * Returns the Pokemon of the least powerful Pokemon in this BST.
-	 * 
-	 * @return the Pokemon of the least powerful Pokemon in this BST and null if this tree
-	 *         is empty.
-	 */
-	public Pokemon getLeastPowerfulPokemon() {
-		// TODO Complete the implementation of this method
-	  
-		// Feel free to implement either the iterative OR the recursive version of this
-		// method.
-	  
-		// If you choose recursion to implement this behavior, add a private helper
-		// method and call it here.
-	  
-		return null; // added to let this incomplete code compile
-	}
+    aggregator += current.getPokemon().toString() + "\n";
 
-	/**
-	 * Returns the Pokemon of the most powerful Pokemon in this BST.
-	 * 
-	 * @return the Pokemon of the most powerful Pokemon in this BST, and null if this tree
-	 *         is empty.
-	 */
-	public Pokemon getMostPowerfulPokemon() {
-		// TODO Complete the implementation of this method
-	  
-		// Feel free to implement either the iterative OR the recursive version of this
-		// method.
-	  
-		// If you choose recursion to implement this behavior, add a private helper
-		// method and call it here.
-	  
-		return null; // added to let this incomplete code compile
-	}
+    // right subtree
+    if (current.getRightChild() != null)
+      aggregator += toStringHelper(current.getRightChild());
+
+    return aggregator;
+  }
+
+  /**
+   * Get a String representation of all the Pokemons stored within this BST in the increasing order,
+   * separated by a newline "\n".
+   * <p>
+   * For instance: "[Pikachu CP:123 (A:1 S:2 D:3)]" + "\n" + "[Eevee CP:224 (A:2 S:2 D:4)]" + "\n" +
+   * [Lapras CP:735 (A:7 S:3 D:5)] + "\n" + "[Mewtwo CP:999 (A:9 S:9 D:9)]" + "\n"
+   * 
+   * @return a String representation of all the Pokemons stored within this BST sorted in an
+   *         increasing order with respect to the CP values. Returns an empty string "" if this BST
+   *         is empty.
+   */
+  public String toString() {
+    if (isEmpty())
+      return "";
+
+    return toStringHelper(root); // call recursive helper on root tree
+  }
+
+  /**
+   * Search for a Pokemon (Pokemon) given the CP value as lookup key.
+   * 
+   * @param cp combat power of a Pokemon
+   * @return the Pokemon whose CP value equals our lookup key.
+   * @throws a NoSuchElementException with a descriptive error message if there is no Pokemon found
+   *           in this BST having the provided CP value
+   */
+  public Pokemon lookup(int cp) {
+    if (isEmpty())
+      throw new NoSuchElementException("No pokemon found: empty BST");
+    return lookupHelper(cp, root);
+  }
+
+  /**
+   * Recursive helper method to lookup a Pokemon given a reference Pokemon with the same CP in the
+   * subtree rooted at current
+   * 
+   * @param find    a reference to a Pokemon target we are lookup for a match in the BST rooted at
+   *                current.
+   * @param current "root" of the subtree we are looking for a match to find within it.
+   * @return reference to the Pokemon stored stored in this BST which matches find.
+   * @throws NoSuchElementException with a descriptive error message if there is no Pokemon whose CP
+   *                                value matches target value, stored in this BST.
+   */
+  public static Pokemon lookupHelper(int cp, PokemonNode current) {
+    // compare cp value to current pokemon stats
+    int comparison = current.getPokemon().getCP() - cp;
+
+    // base case: current node's pokemon equals cp
+    if (comparison == 0)
+      return current.getPokemon();
+
+    // base case: no further subtrees to lookup in
+    if (comparison > 0 && current.getLeftChild() == null
+        || comparison < 0 && current.getRightChild() == null)
+      throw new NoSuchElementException("No pokemon found in BST matching the provided CP value");
+
+    // reduction case: search in the appropriate subtrees
+    return (comparison > 0) ? lookupHelper(cp, current.getLeftChild())
+        : lookupHelper(cp, current.getRightChild());
+  }
+
+  /**
+   * Computes and returns the height of this BST, counting the number of nodes (PokemonNodes) from
+   * root to the deepest leaf.
+   * 
+   * @return the height of this Binary Search Tree
+   */
+  public int height() {
+    if (isEmpty())
+      return 0;
+
+    return heightHelper(root);
+  }
+
+  /**
+   * Recursive helper method that computes the height of the subtree rooted at current
+   * 
+   * @param current pointer to the current PokemonNode within a PokemonTree
+   * @return height of the subtree rooted at current, counting the number of PokemonNodes
+   */
+  public static int heightHelper(PokemonNode current) {
+    // base case: null subtree
+    if (current == null)
+      return 0;
+
+    // reduction case: pick the largest height of subtrees
+    return 1
+        + Math.max(heightHelper(current.getLeftChild()), heightHelper(current.getRightChild()));
+  }
+
+  /**
+   * Find the Pokemon of the least powerful Pokemon in this BST.
+   * 
+   * @return the Pokemon of the least powerful Pokemon in this BST and null if this tree is empty.
+   */
+  public Pokemon getLeastPowerfulPokemon() {
+    if (isEmpty())
+      return null;
+
+    PokemonNode n = root; // current node
+    while (n.getLeftChild() != null)
+      n = n.getLeftChild();
+
+    return n.getPokemon(); // pokemon at left-most position in the BST
+  }
+
+  /**
+   * Find the Pokemon of the most powerful Pokemon in this BST.
+   * 
+   * @return the Pokemon of the most powerful Pokemon in this BST, and null if this tree is empty.
+   */
+  public Pokemon getMostPowerfulPokemon() {
+    if (isEmpty())
+      return null;
+
+    PokemonNode n = root; // current node
+    while (n.getRightChild() != null)
+      n = n.getRightChild();
+
+    return n.getPokemon(); // pokemon at right-most position in the BST
+  }
 
 }
