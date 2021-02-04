1f37ee2067fc9c8a8f96443e138e278ba9b91f41
Safi <nassarsafi@gmail.com>
2021-02-04 19:59:46 +0200

    ✅ [cs537, P1] Complete implementation of look command

--- [cs537, P1] Unix utilities/my-look.c
+++ [cs537, P1] Unix utilities/my-look.c
@@ -1,9 +1,7 @@
 /**
  * @file my-look.c
  * @brief a simplified implementation of 'look' command.
- *
- * @author Safi
- * @copyright Copyright (c) 2021
+ * @copyright Copyright (c) 2021 by Safi Nassar
  */
 
 #include <assert.h>
@@ -17,75 +15,84 @@ extern int errno;  // error number
 extern char *optarg;
 extern int optind, opterr, optopt;
 
-#define DEBUG false  // debug flag
-// messages defined in specification
+// user-interface messages:
 #define MESSAGE_UTILITY_INFO "my-look from CS537 Spring 2021"
+#define MESSAGE_USAGE_INFO                                            \
+  "usage: `my-look [<options>] [-f <filename>] <string to search>`\n" \
+  "\t-f   file path of dictionary\n"                                  \
+  "\t-V   information about utility\n"                                \
+  "\t-h   documentation information`"
 #define MESSAGE_ARG_ERROR "my-look: invalid command line"
 #define MESSAGE_FILE_ERROR "my-look: cannot open file"
 
-/* note: assuming input lines are a subset of those in /usr/share/dict/words
- * (longest line ~30 chars) */
+/* note: assuming input lines are a subset of those in
+ * /usr/share/dict/words (longest line ~30 chars) */
 static const size_t BUFFER_SIZE = 128;  // maximum line length
+// program configuration parameters controlling input/output and behavior
 struct Config {
+  // program subroutines
   enum { COMPARE, USAGE, INFO } functionality;
+  // input & output params
   struct {
-    char *prefix;  // input string to search for
-    FILE *target;  // input target stream
+    char *prefix;  // search string input
+    FILE *target;  // dictionary input stream
     FILE *output;  // result output stream
   } variable;
 };
 
-static void cliAdapter(int argc, char ***argv, struct Config *config);
-static void findPrefixedLine(char *prefix, FILE *target, FILE *output);
+/* function definitions */
+static void cliAdapter(int argc, char ***argv, struct Config *param);
+static void findPrefixedLine(char *str, FILE *in, FILE *out);
 static void printInformation();
 static void printDocumentation();
 
 /**
- * program objective: find all lines beginning with a prefix
+ * find all dictionary lines beginning with a prefix string, & prints them to
+ * the user
  *
- * functionality:
+ * *functionality:*
  * [core]
- *  → compare target lines with prefix string
+ * → compare target lines with prefix string
  *      [input variables]
- *          - search target string lines
- *          - prefix string
+ *      - search target string lines
+ *      - prefix string
  *      [output result]
- *          - matching string lines
+ *      - matching string lines
  * [auxiliary]
- *  → provide information about program
- *  → provide usage documentation
+ * → provide information about program
+ * → provide usage documentation
  *
- * supported behavior:
+ * *supported behavior:*
  * [comparison]
- *  → case insensitive only
+ * → case insensitive only
  * [input data type/format]
- *  → ASCII characters only
- *  → words starting with "-" unsupported
+ * → ASCII characters only
+ * → words starting with "-" unsupported
  * [input source for search target variable]
- *  → standard input stream
- *  → file stream
+ * → standard input stream
+ * → file stream
  * [output result target]
- *  → standard output stream
+ * → standard output stream
  *
- * user interface:
- *  → command-line
+ * *user interface:*
+ * → command-line
  *      [option flags & values]
- *          -V prints utility information
- *          -h prints help usage documentation
- *          -f reads <filename> as the input dictionary stream
+ *      -V prints utility information
+ *      -h prints help usage documentation
+ *      -f reads <filename> as the input dictionary stream
  *
- * @param argc
- * @param argv
+ * @param argc command-line argument count
+ * @param argv command-line argument value
  * @return int program exit code
  */
 int main(int argc, char *argv[]) {
-  // set default variables and program configurations
+  // set default variables and program configuration parameters
   struct Config config = {COMPARE, {NULL, stdin, stdout}};
 
-  // parse command-line arguments: set variables and choose mode
+  // parse CLI arguments: update configuration of variables & execution function
   cliAdapter(argc, &argv, &config);
 
-  // choose function to execute
+  // execute requested functionality
   switch (config.functionality) {
     case COMPARE:
       findPrefixedLine(config.variable.prefix, config.variable.target,
@@ -99,8 +106,9 @@ int main(int argc, char *argv[]) {
       break;
   }
 
+  // clean up
   if (fclose(config.variable.target) != 0) {
-    printf("Error while closing the file.\n");
+    printf("my-look: cannot close file\n");
     exit(1);
   }
 
@@ -108,30 +116,33 @@ int main(int argc, char *argv[]) {
 }
 
 /**
- * pick mode & associated variables
+ * parse CLI arguments: handle associated input variables and update program
+ * config
  *
+ * @param argc cli count
+ * @param argv pointer to cli value vector
+ * @param config program's behavior configuration
  */
 static void cliAdapter(int argc, char ***argv, struct Config *config) {
-  int nextOption;
-  opterr = 0;  // suppress printing errors
+  int nextOption;  // option key
+  opterr = 0;      // suppress printing errors
 
-  // parse option arguments:
-  // if multiple arguments provided it will process the first argument only.
+  // parse option & non-option arguments:
   while ((nextOption = getopt(argc, *argv, "-Vhf:")) != -1) {
     switch (nextOption) {
       case 'V':
         config->functionality = INFO;
         return;
       case 'h':
-        config->functionality = INFO;
+        config->functionality = USAGE;
         return;
       case 'f':
         config->functionality = COMPARE;
+        // create requested file stream
         if ((config->variable.target = fopen(optarg, "r")) == NULL)
           goto fileError;
         break;
-      case '\1':
-        // parse non-option argument:
+      case '\1':  // parse non-option arguments
         config->variable.prefix = optarg;
         goto verifyArgumentCount;
       case '?':
@@ -156,29 +167,22 @@ fileError:
 }
 
 /**
- * input data values:
- * - prefix string
- * - target string
+ * compare (case-insensitive) each line in a stream to a prefix string, and
+ * print those matching
  *
- * result value:
- * - result string
- *
- * limitations:
- * - search comparisons are case-insensitive
- *
- * @param string to search for
+ * @param prefix string to search for
  * @param target dictionary (list of words)
- * @param output stream (by default stdout)
+ * @param output stream to print results
  */
 static void findPrefixedLine(char *prefix, FILE *target, FILE *output) {
   char buffer[BUFFER_SIZE];  // read-in buffer
 
   // read input line by line
   while (fgets(buffer, BUFFER_SIZE, target) != NULL) {
+    // check if line begins with prefix
     if (strncasecmp(prefix, buffer, strlen(prefix)) == 0) {
-      char *saveptr;
-      // remove new line
-      char *token = strtok_r(buffer, "\n", &saveptr);
+      char *saveptr;  // irrelevant (for repeated calls)
+      char *token = strtok_r(buffer, "\n", &saveptr);  // remove new line
       fprintf(output, "%s\n", token);
     }
   }
@@ -192,7 +196,4 @@ static void printInformation() { printf("%s\n", MESSAGE_UTILITY_INFO); }
 /**
  * print usage documentation
  */
-static void printDocumentation() {
-  printf("%s\n%s\n%s\n", "usage: `my-look [-f <filename>] <string to search>`",
-         "info: `my-look -h`", "doc: `my-look -V`");
-}
+static void printDocumentation() { printf("%s\n", MESSAGE_USAGE_INFO); }
