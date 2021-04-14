cbe8f50ad26be4d249851aa385daf067f31bbd20
Safi <nassarsafi@gmail.com>
2021-04-15 02:30:00 +0300

    🚚 [cs537, P6] Migrate implementation from P5.

--- [cs537, P6] P6 xv6 Memory Encryption, Kernel Version/src/userProgram.c
+++ [cs537, P6] P6 xv6 Memory Encryption, Kernel Version/src/userProgram.c
@@ -1,6 +1,13 @@
 
-// TODO: boilerplate code from previous assignment.
-int main() {
+#include "param.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "ptentry.h"
+
+#define PGSIZE 4096
+
+int main(void) {
     char *ptr = sbrk(PGSIZE);  // Allocate one page
     struct pt_entry pt_entry;
     // Get the page table information for newly allocated page
@@ -9,4 +16,5 @@ int main() {
     ptr[0] = 0x0;
     // The page should now be decrypted and put into the clock queue
     getpgtable(&pt_entry, 1, 1);
+    exit();
 }
