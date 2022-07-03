ca8837994aa9ffc76d8a722c786fd24fccbea461
Safi <nassarsafi@gmail.com>
2022-07-02 23:28:21 -0500

    🔨 CMake configuration.

---
+++ cpp/build/CMakeFiles/cppProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppProject.dir/main.cpp.o"
+  "CMakeFiles/cppProject.dir/main.cpp.o.d"
+  "cppProject"
+  "cppProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int, char**) {
+  int x = (10 <=> 20) > 0;
+
+  std::cout << x << std::endl;
+  std::cout << "Hello, world!\n";
+  std::cout << "Hello, world!\n";
+  std::cout << "Hello, world!\n";
+  std::cout << "Hello, world!\n";
+  std::cout << "Hello, world!\n";
+}
