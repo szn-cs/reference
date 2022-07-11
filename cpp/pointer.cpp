60108f1b7a5282f5d2812472638d213da6abe7eb
Safi <nassarsafi@gmail.com>
2022-07-10 21:48:09 -0500

    ➕ Upload C++ examples

--- cpp/pointer.cpp
+++ cpp/pointer.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+void func1();
+
 using namespace std;
 
 int main(int, char **) {
@@ -10,5 +12,24 @@ int main(int, char **) {
   p = &var;
 
   cout << *p << endl;
+
+  func1();
+
   return 0;
+}
+
+void func1() {
+  cout << "> func1" << endl;
+
+  const char *const arr[]{"sentence1", "sentence2", "sentence3"};
+
+  for (const char *element : arr) {
+    cout << element << endl;
+  }
+  for (int i{0}; i < size(arr); ++i) {
+    cout << arr[i] << endl;
+    cout << *arr[i] << endl;
+  }
+
+  return;
 }
