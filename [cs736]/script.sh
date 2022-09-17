fa927d3dc5503daea8edb687b7ae953836bc8bf7
Safi <nassarsafi@gmail.com>
2022-09-16 20:38:18 -0500

    clean

--- [cs736]/script.sh
+++ [cs736]/script.sh
@@ -3,14 +3,18 @@
 - Disk image to boot kernel from http://fs.devloop.org.uk/   Gentoo
 - required system tools: https://docs.kernel.org/process/changes.html#changes 
 - User-Mode Linux (UML): run Linux as process http://user-mode-linux.sourceforge.net/source.html
+  - http://user-mode-linux.sourceforge.net/
 - Create virtual disks http://howto.gumph.org/content/get-started-with-user-mode-linux/
 
 ## current systems config file
 zcat /proc/config.gz
 
-## kernel
+## kernel & disk
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.9.tar.xz
+wget http://fs.devloop.org.uk/filesystems/Gentoo/Gentoo-AMD64-root_fs.bz2
 tar xvf linux-5.19.9.tar.xz
+mv Gentoo-AMD64-root_fs.bz2 root_fs
+bzip2
 
 # tools
 # sudo apt install gcc clang make bash ld flex bison pahole fdformat depmod e2fsck fsck.jfs reiserfsck xfs_db mksquashfs btrfsck pccardctl quota pppd showmount ps udevd grub mcelog iptables openssl bc sphinx cpio 
@@ -23,11 +27,14 @@ make menuconfig ARCH=um # choose x64, ext2 (uncheck use ext4 for ext2), ext3, ex
 make ARCH=um # generates a UML binary
 ls -l linux
 
-## download Gentoo 
-wget http://fs.devloop.org.uk/filesystems/Gentoo/Gentoo-AMD64-root_fs.bz2
-mv Gentoo-AMD64-root_fs.bz2 root_fs
-sudo ./linux mem=256m
+
+#RUN kernel
+sudo ./linux ubda=root_fs mem=256m
 # log in root with no password
 
-# if issues arise clear up build 
+# if issues arise because of configuration clear up build and try again.
 make mrproper
+make mrproper ARCH=um
+
+
+
