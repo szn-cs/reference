16cec8b9fd61efe63d9f31401a21eb8dfbd42d08
safi <nassarsafi@gmail.com>
2023-02-11 19:58:45 -0600

    move projects to own repo

--- [cs354, P4A] Cache performance/pinCommand.sh
+++
@@ -1,8 +0,0 @@
-# pin v3.16
-#/s/pin-3.16/pin -injection child -t /s/pin-3.16/source/tools/Memory/obj-ia32/dcache.so -c <capacity> -a <associativity> -b <block-size> -o <output-file> -- <your-exe>
-# D-cache (data as opposed to instruction cache) - default params: 
-# capacity: 2  (2KB or 2048 bytes)
-# associativity: 1   (direct-mapped)
-
-/s/pin-3.16/pin -injection child -t /s/pin-3.16/source/tools/Memory/obj-ia32/dcache.so -c 2 -a 1 -b 64 -o output -- $1
-
--- [cs354, P4B] Cache  simulator/execute.sh
+++
@@ -1,6 +0,0 @@
-tracePath="traces/trace2"
-s=4; 
-E=1; 
-b=4; 
-./csim-ref -v -s $s -E $E -b $b -t $tracePath; 
-./csim -v -s $s -E $E -b $b -t $tracePath
