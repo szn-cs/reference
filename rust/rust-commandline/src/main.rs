0d6f7e0d5109617c0788ac39cea34389e7e68de8
Safi <nassarsafi@gmail.com>
2023-03-16 08:58:29 -0500

    rust commandline

--- rust/rust-commandline/src/main.rs
+++ rust/rust-commandline/src/main.rs
@@ -1,20 +1,23 @@
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path;
 
-// (source ./script.sh && run)
 fn main() {
+    // parse arguments
     let arguments: Vec<String> = env::args().collect();
+    let (_pattern, filename) = parse_config(&arguments);
 
-    dbg!(&arguments);
-
-    let pattern: &String = &arguments[1];
-    let filename: &String = &arguments[2];
-
-    println!("pattern: {}\nfilename: {}", pattern, filename);
-
-    let path = Path::new(filename);
+    let path = path::Path::new(filename);
     let contents: String = fs::read_to_string(path).expect("Unable to read file.");
 
-    println!("Contents:\n{}", contents);
+    println!("Contents: {}", contents);
 }
+
+fn parse_config(arguments: &[String]) -> (&str, &str) {
+    let pattern = &arguments[1];
+    let filename = &arguments[2];
+
+    (pattern, filename)
+}
+
+// https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#grouping-configuration-values
